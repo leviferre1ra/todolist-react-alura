f11014e541e0a80cdccb3a6d33b74195bd52595a
leviferre1ra <leviferreira1677@gmail.com>
2026-02-23 12:01:22 -0300

    first commit

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef } from 'react';
+import { IconClose } from '../icons';
+import './dialog.style.css'
+
+export function Dialog({ isOpen, onClose, children }) {
+    // não deveríamos fazer buscas no DOM desse jeito:
+    // const dialog = document.querySelector("dialog");
+
+    const dialogRef = useRef(null)
+
+    useEffect(() => {
+        console.log('Deveríamos mostrar a modal?', isOpen)
+        if (isOpen) {
+            openDialog()
+        } else {
+            closeDialog()
+        }
+    }, [isOpen])
+
+    // "Show the dialog" button opens the dialog modally
+    const openDialog = () => {
+        dialogRef.current.showModal();
+    };
+
+    // "Close" button closes the dialog
+    const closeDialog = () => {
+        dialogRef.current.close();
+    };
+
+    return (
+        <>
+            <dialog ref={dialogRef} className='dialog'>
+                <div className='btn-close-wrapper'>
+                    <button autoFocus onClick={onClose} className='btn-close'>
+                        <IconClose />
+                    </button>
+                </div>
+
+                {children}
+            </dialog>
+        </>
+
+    )
+}
